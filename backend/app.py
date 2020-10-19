b4e80182e53475db4dc0b0ec0eb33fb05ee7c0db
Pritish Dugar <pritishdugar509@gmail.com>
2020-10-19 19:26:37 +0530

    sign up and login tested

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 import firebase_admin
 from firebase_admin import credentials, auth
+from functools import wraps
+from flask_cors import CORS
 import requests
 import pyrebase
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 # Initialize connection to firebase
-pb = pyrebase.initialize_app(json.load(open('./fbconfig.json')))
+pb = pyrebase.initialize_app(json.load(open('backend/fbconfig.json')))
 auth1 = pb.auth()
 
 # Admin SDK
-cred = credentials.Certificate(json.load(open('./fbAdminConfig.json')))
+cred = credentials.Certificate(json.load(open('backend/fbAdminConfig.json')))
 default_app = firebase_admin.initialize_app(cred)
 
 def userId_required(f):
@@ -46,24 +49,23 @@ def userId_required(f):
 # Index
 @app.route('/')
 def index():
-    pass
+    return "Index"
 
 # route to sign a new user up  
 @app.route('/signup', methods=["POST"])
 def signup():
-    if request.method == "POST":
+    if request.method:
         email = request.form["signup_email"]
         password = request.form["signup_password"]
         try:
-            user = auth1.create_user_with_email_and_password(email, password, n)
+            user = auth1.create_user_with_email_and_password(email, password)
         except:
-            return Response(status=400)
+            return Response(status=401)
+    return Response(status=200)
 
-return Response(status=400)
-
-@app.route('/login', methods=["POST", "GET"])
+@app.route('/login', methods=["POST"])
 def login():
-    if request.method == "POST":
+    if request.method:
         email = request.form["login_email"]
         password = request.form["login_password"]
         try:
@@ -72,9 +74,12 @@ def login():
             user_id = user['idToken']
         except:
             return Response(status=400)
+    return Response(status=200)
 
-    return Response(status=400)
-
+@app.route('/home')
+@userId_required
+def home():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
